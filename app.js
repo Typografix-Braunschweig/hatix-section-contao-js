ae77b56ee97aef58715f74cf9fe87a1a9ea976ff
AP_14 <ap_14@ap_14.typografix.net>
2023-09-01 16:14:55 +0200

    finished

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const hatixKarte = document.querySelector("#hatix-karte");
 const hatixKarteParent = document.querySelector("#hatix-karte").parentNode;
 
 const newDivHintergrund = document.createElement("div");
-newDivHintergrund.classList.add("mod_article", "slideshow", "blue", "block", "custom-hinterground-1");
+newDivHintergrund.classList.add("mod_article", "slideshow", "light-blue", "block", "custom-hinterground-1");
 
 hatixKarteParent.insertBefore(newDivHintergrund, hatixKarte);
 
@@ -14,6 +14,20 @@ newDivTextWrapper.classList.add("custom-text-wrapper");
 newDivHintergrund.appendChild(newDivTextWrapper);
 
 const newTitle = document.createElement("h1");
-newTitle.classList.add("new-title-h1");
+newTitle.classList.add("new-title-h1", "ce_headline", "lightblue");
 newTitle.textContent = "Harzweitmobil im Landkreis Goslar und Göttingen";
 newDivTextWrapper.appendChild(newTitle);
+
+const newTextContent = document.createElement("p");
+newTextContent.classList.add("new-textcontent-p", "ce_text", "intro");
+newTextContent.innerHTML += `Im Landkreis Goslar und Göttingen führt ein <a href="https://www.harz-nah-dran.de/harzweitmobil.html" class="custom-href-text">zum interaktiven Linien</a> online zur nächsten Haltestelle und zeigt die nächsten Abfahrten an.`;
+newDivTextWrapper.appendChild(newTextContent);
+
+const newButtonTextWrapper = document.createElement("div");
+newButtonTextWrapper.classList.add("new-button-text");
+newButtonTextWrapper.innerHTML += `
+<a href="https://www.harz-nah-dran.de/harzweitmobil.html">
+    <img src="https://www.harz-nah-dran.de/files/vsn/images/layout/hyperlink.png"/>
+    <span>Zum interaktiven Liniennetzplan</span>
+</a>`;
+newDivTextWrapper.appendChild(newButtonTextWrapper);
